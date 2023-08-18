16e7faedaa17b027855d7aa56f10a3c6744bf297
amirhossein <amirhosseinghodratnema@gmail.com>
2023-08-18 15:15:35 +0330

    Create home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import AllCars from "@/components/module/AllButton";
+import Category from "@/components/module/category";
+import SearchBar from "@/components/module/searchBar";
 import CarPage from "@/components/templates/carPage";
 import carsData from "@/data/carData";
 
 export default function Home() {
+  const cars = carsData.slice(0, 3);
   return (
-    <div></div>
+    <div>
+      <SearchBar />
+      <Category />
+      <AllCars />
+      <CarPage data={cars} />
+    </div>
   )
 }
